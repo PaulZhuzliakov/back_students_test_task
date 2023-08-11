981c90abfb9a865a31d4322c3fc9ac27fe237675
PaulZhuzliakov <VirginiaBeach$1984>
2023-08-11 19:28:33 +0300

    не работала конфигурация "spring.jackson.property-naming-strategy=SNAKE_CASE испралено

--- src/main/java/com/example/back_students_test_task/config/WebConfig.java
+++ src/main/java/com/example/back_students_test_task/config/WebConfig.java
@@ -1,15 +1,35 @@
 package com.example.back_students_test_task.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
-@EnableWebMvc
+
 public class WebConfig implements WebMvcConfigurer{
+
+    @Autowired
+    private ObjectProvider<HttpMessageConverters> messageConvertersProvider;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    /**
+     * аннотация @EnableWebMvc делает неактивной конфигурацию "spring.jackson.property-naming-strategy=SNAKE_CASE"
+     * чтобы стратегия наименования полей при сериализации работала, пришлось переопределить этот метод
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        this.messageConvertersProvider
+                .ifAvailable((customConverters) -> converters.addAll(customConverters.getConverters()));
+    }
+
 }
